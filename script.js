eed82667328b613d6da6798fbe18019562632059
NathanC90 <ymnchao@gmail.com>
2023-12-08 21:52:18 +0800

    Add a function to get player's choice

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let choice1 = 'Rock';
 let choice2 = 'Paper';
 let choice3 = 'Scissors';
+let playerChoice = prompt("Please enter 'Rock', 'Paper', or 'Scissors.'");
 
 function getComputerChoice(){
     let choiceNumber = Math.floor(Math.random() * 3 + 1);
@@ -21,4 +22,11 @@ function getComputerChoice(){
             console.log("");
             break;
     }
+}
+
+function getPlayerChoice(playerChoice){
+    let firstLetter = playerChoice.charAt(0).toUpperCase();
+    let otherLetters = playerChoice.slice(1).toLowerCase();
+    let finalResult = firstLetter + otherLetters;
+    console.log(finalResult);
 }
