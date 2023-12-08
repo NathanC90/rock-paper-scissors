c5467b35cbb7c5396b5f67d2d732a7b7fe4a22c2
NathanC90 <ymnchao@gmail.com>
2023-12-08 16:37:55 +0800

    Add a function regarding computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+let choice1 = 'Rock';
+let choice2 = 'Paper';
+let choice3 = 'Scissors';
+
+function getComputerChoice(){
+    let choiceNumber = Math.floor(Math.random() * 3 + 1);
+    switch (choiceNumber) {
+        case 1:
+            console.log(choice1);
+            break;
+        
+        case 2:
+            console.log(choice2);
+            break;
+        
+        case 3:
+            console.log(choice3);
+            break;
+    
+        default:
+            console.log("");
+            break;
+    }
+}
