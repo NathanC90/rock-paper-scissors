05c4d404bb1ee75a83ddd669465cea2d25564bb2
NathanC90 <ymnchao@gmail.com>
2023-12-08 22:13:30 +0800

    Add the third function to compare player and computer choices

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ function getComputerChoice(){
     let choiceNumber = Math.floor(Math.random() * 3 + 1);
     switch (choiceNumber) {
         case 1:
-            console.log(choice1);
+            console.log(`Computer's Choice is ${choice1}.`);
+            return choice1;
             break;
         
         case 2:
-            console.log(choice2);
+            console.log(`Computer's Choice is ${choice2}.`);
+            return choice2;
             break;
         
         case 3:
-            console.log(choice3);
+            console.log(`Computer's Choice is ${choice3}.`);
+            return choice3;
             break;
     
         default:
@@ -24,9 +27,32 @@ function getComputerChoice(){
     }
 }
 
-function getPlayerChoice(playerChoice){
+ function getPlayerChoice(playerChoice){
     let firstLetter = playerChoice.charAt(0).toUpperCase();
     let otherLetters = playerChoice.slice(1).toLowerCase();
     let finalResult = firstLetter + otherLetters;
     console.log(finalResult);
+    return finalResult;
 }
+
+function compete(playerSelection, computerSelection) {
+    if(playerSelection == 'Rock' && computerSelection == 'Paper') {
+        console.log("You lose! Paper beats Rock. Reload the page to play again.");
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        console.log("You win! Rock beats Scissors. Reload the page to play again.");
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log("You win! Paper beats Rock. Reload the page to play again.");
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        console.log("You lose! Scissors beat Paper. Reload the page to play again.");
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        console.log("You lose! Rock beats Scissors. Reload the page to play again.");
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        console.log("You win! Scissors beat Paper. Reload the page to play again.");
+    } else if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    } else {
+        console.log("Invalid input. Please ensure you have entered 'Rock', 'Paper', or 'Scissors'. Reload the page to play again.");
+    }
+}
+
+compete(getPlayerChoice(playerChoice), getComputerChoice());
